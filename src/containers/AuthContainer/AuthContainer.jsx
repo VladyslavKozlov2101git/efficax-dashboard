4a434ab9791044648b8e3b2d5569bbce884da9b8
VladyslavKozlov2101git <60036579+VladyslavKozlov2101git@users.noreply.github.com>
2022-11-16 20:16:15 +0200

    Routes Settings

---
+++ src/containers/AuthContainer/AuthContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SideBg from '../../layout/SideBg';
+import AuthRoutes from '../../routes/AuthRoutes';
+
+import './AuthContainer.scss';
+
+const AuthContainer = () => {
+  return (
+    <main className="auth-page">
+      {/* <SideBg /> */}
+      <div className="auth-page__content">{/* <AuthRoutes /> */}</div>
+    </main>
+  );
+};
+
+export default AuthContainer;
---
+++ src/routes/paths.js
@@ -0,0 +1,10 @@
+export const rootAuthPath = '/auth';
+export const rootMainPath = '/main';
+
+export const authPath = {
+  signIn: `${rootAuthPath}/sign-in`,
+};
+
+export const mainPath = {
+  dashboardProjects: { path: `${rootMainPath}/projects`, pathName: 'Projects' },
+};
