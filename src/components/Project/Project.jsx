1acbf11e3a94a87e7d3640e0ebcdb82dd4f149f5
VladyslavKozlov2101git <vladislavkozlov2101git@gmail.com>
2022-12-13 10:05:19 +0200

    MAP Method

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -8,13 +8,32 @@ const Project = () => {
   //Повинен бути лише 1 корневий елемент
   const arrProjects = [
     {
-      id: "8d98ca66-147d-42ef-805d-0c0c9a718de4",
-      name: "Hello Back",
+      id: "1",
+      name: "Hello Back 1",
+      key: "TP-1",
+      pub_date: "2022-12-09T12:27:56.328820Z",
+    },
+    {
+      id: "2",
+      name: "Hello Back 2",
       key: "TP-2",
-      pub_date: "2022-12-09T12:27:56.328820Z"
-  },
-   
+      pub_date: "2022-12-09T12:27:56.328820Z",
+    },
+    {
+      id: "3",
+      name: "Hello Back 3",
+      key: "TP-3",
+      pub_date: "2022-12-09T12:27:56.328820Z",
+    },
+    {
+      id: "4",
+      name: "Hello Back 4",
+      key: "TP-4",
+      pub_date: "2022-12-09T12:27:56.328820Z",
+    },
   ];
+
+
 
   return (
     <div className="project">
@@ -24,10 +43,10 @@ const Project = () => {
         <ProjectElement />
         <ProjectElement />
         <ProjectElement />
+       
       </div>
       <h2 className="project__title uppercase">All projects </h2>
       <div className="project__column">
-
         {/* //Є масив об'єктів arrProjects, який прийшов з бека. У ньому є 3 елементи списка.
          Метод map перебирає кожен едемент і якщо він задовілбняю умови (у даному випадку умов у нас немає),
           то ми вертаємо (виводимо) цей елемент.
@@ -35,11 +54,21 @@ const Project = () => {
 
 
          ДОМАШНЄ ЗАВДАННЯ.  Беки вже зробили запрос на створення проектів. Попробувать
+
+         Метод map() визивається стільки ж разів, скільки у тебе елементів в масиві.
+
+         Index -- порядковий номер в списку, він починається з 0.
+
           
           */}
 
-        {arrProjects.map(elementSpiska => (
-          <ProjectElementRow />
+
+
+        {arrProjects.map((el, index) => (
+
+          <ProjectElementRow el={el} key={index} />
+
+          
         ))}
 
 
--- src/components/ProjectElementRow/ProjectElementRow.jsx
+++ src/components/ProjectElementRow/ProjectElementRow.jsx
@@ -2,14 +2,15 @@ import React from "react";
 
 import "./ProjectElementRow.scss";
 
-const ProjectElementRow = () => {
+const ProjectElementRow = ({el}) => {
+  console.log(el)
   return (
     <div className="projectElementRow">
         <div className="projectElementRow__logo">
-          <div className="projectElementRow__logo--title">ZIRA</div>
+          <div className="projectElementRow__logo--title">{el.key}</div>
         </div>
         <div className="projectElementRow__ceil">
-         <p className="projectElementRow__ceil--title">Zira Board</p>
+         <p className="projectElementRow__ceil--title">{el.name}</p>
          <p className="projectElementRow__ceil--description">A Task Manager you can’t refuse</p>
         </div>
         <div className="projectElementRow__ceil">
@@ -21,7 +22,7 @@ const ProjectElementRow = () => {
          <p className="projectElementRow__ceil--description">Project lead</p>
         </div>
         <div className="projectElementRow__ceil">
-         <p className="projectElementRow__ceil--title">203</p>
+         <p className="projectElementRow__ceil--title">{el.id}</p>
          <p className="projectElementRow__ceil--description">Total tasks</p>
         </div>
         <div className="projectElementRow__ceil">
